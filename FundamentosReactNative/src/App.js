828f9a138695339e5a0aedd5d5e5b2b5fe9af81a
Cesar Amorim <cesar_famorim@hotmail.com>
2022-04-25 18:30:09 -0300

    RenderizacaoCondicional

--- FundamentosReactNative/src/App.js
+++ FundamentosReactNative/src/App.js
@@ -8,14 +8,33 @@ import Aleatorio from './components/Aleatorio'
 import Titulo from './components/Titulo' 
 import Botao from './components/Botao' 
 import Contador from './components/Contador' 
-/*import Pai from './components/direta/Pai'*/
-import Pai from './components/indireta/Pai'
+import Pai from './components/direta/Pai'
+/*import Pai from './components/indireta/Pai'*/
+import ContadorV2 from './components/contador/ContadorV2'
+import DiferenciarAndroidIOS from './components/DiferenciarAndroidIOS'
+import ParImpar from './components/ParImpar'
+import Familia from './components/relacao/Familia'
+import Membro from './components/relacao/Membro'
+import IfUsuarioLogado from './components/IfUsuarioLogado'
 
 export default () => (
 <SafeAreaView style = {style.App}>
+    <IfUsuarioLogado usuario = {{nome: 'Gui', email: 'gui@gui.com'}}/>
+
+    {/*<Familia>
+        <Membro nome = "Bia" sobrenome = "Arruda"></Membro>
+        <Membro nome = "Bia" sobrenome = "Arruda"></Membro>
+    </Familia>
+    <Familia>
+        <Membro nome = "Ana" sobrenome = "Silvia"></Membro>
+        <Membro nome = "Gui" sobrenome = "Silvia"></Membro>
+    </Familia>
+
+    <ParImpar num = {2}/>
+    <DiferenciarAndroidIOS />
+    <ContadorV2 />
+    <Pai />   
     <Pai />
-    {
-    /*<Pai />
     <Contador inicial = {100} passo ={5}/>
     <Contador />
     <Botao />
---
+++ FundamentosReactNative/src/components/relacao/Familia.js
@@ -0,0 +1,12 @@
+import React from  'react'
+import { Text } from 'react-native'
+
+export default props => {
+    return(
+        <>
+        <Text> [Inicio] Membros da Familia: </Text>
+            {props.children}
+        <Text> [Fim] Membros da Familia.</Text>
+        </>
+    )
+}
