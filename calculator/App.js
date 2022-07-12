4d8d261f378488c470a7715d58ddee2a4ce82bb1
Cesar Amorim <cesar_famorim@hotmail.com>
2022-07-12 20:45:02 -0300

    Calculator #2

--- calculator/App.js
+++ calculator/App.js
@@ -3,31 +3,50 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 
 
 export default class App extends Component {
+  state = {
+    displayValue: '0'
+  }
+  
+  addDigit = n => {
+    this.setState({ displayValue: n})
+  }
+
+  clearMemory = () => {
+    this.setState({ displayValue: '0' })
+  }
+
+  setOperation = operation => {
+
+  }
+  
+  
   render () {
     return (
       <View style={styles.container}>
+        <Display value = {this.state.displayValue}></Display>
         <View style={styles.buttons}>
-          <Button label = 'AC'></Button>
-          <Button label = '/'></Button>
-          <Button label = '7'></Button>
-          <Button label = '8'></Button>
-          <Button label = '9'></Button>
-          <Button label = '*'></Button>
-          <Button label = '4'></Button>
-          <Button label = '5'></Button>
-          <Button label = '6'></Button>
-          <Button label = '-'></Button>
-          <Button label = '1'></Button>
-          <Button label = '2'></Button>
-          <Button label = '3'></Button>
-          <Button label = '+'></Button>
-          <Button label = '0'></Button>
-          <Button label = '.'></Button>
-          <Button label = '='></Button>
+          <Button label = 'AC' triples onClick={this.ClearMemory}></Button>
+          <Button label = '/' operation onClick={() => this.setOperation('/')}></Button>
+          <Button label = '7' onClick={() => this.addDigit(7)}></Button>
+          <Button label = '8' onClick={() => this.addDigit(7)}></Button>
+          <Button label = '9' onClick={() => this.addDigit(7)}></Button>
+          <Button label = '*' operation onClick={() => this.setOperation('*')}></Button>
+          <Button label = '4' onClick={() => this.addDigit(7)}></Button>
+          <Button label = '5' onClick={() => this.addDigit(7)}></Button>
+          <Button label = '6' onClick={() => this.addDigit(7)}></Button>
+          <Button label = '-' operation onClick={() => this.setOperation('-')}></Button>
+          <Button label = '1' onClick={() => this.addDigit(7)}></Button>
+          <Button label = '2' onClick={() => this.addDigit(7)}></Button>
+          <Button label = '3' onClick={() => this.addDigit(7)}></Button>
+          <Button label = '+' operation onClick={() => this.setOperation('+')}></Button>
+          <Button label = '0' double onClick={() => this.addDigit(7)}></Button>
+          <Button label = '.' onClick={() => this.addDigit('.')}></Button>
+          <Button label = '=' operation onClick={() => this.setOperation('=')}></Button>
 
         </View>
       </View>
